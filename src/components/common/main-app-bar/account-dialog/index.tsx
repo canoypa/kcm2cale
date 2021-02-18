f0e876bf3b223fae58b9512abcc95be3040eb236
Cano <contact.sasasaky@gmail.com>
2021-02-18 21:16:59 +0900

    ログイン状態の表示

--- src/components/common/main-app-bar/account-dialog/index.tsx
+++ src/components/common/main-app-bar/account-dialog/index.tsx
@@ -1,30 +1,65 @@
+import firebase from "firebase/app";
 import { FC } from "react";
 import { useHistory, useLocation } from "react-router";
+import { firebaseAuth } from "../../../../core/firebase/auth";
+import { useUser } from "../../../../core/firebase/auth/hooks";
 import { Avatar } from "../../avatar";
 import { Button } from "../../button";
 import { Dialog, DialogContent } from "../../dialog";
 import * as styles from "./styles";
+
+type AccountHeaderProps = {
+  user: firebase.User | null;
+  onClose: () => void;
+};
+const AccountHeader: FC<AccountHeaderProps> = ({ user, onClose }) => {
+  const { push } = useHistory();
+  const { pathname } = useLocation();
+
+  const singIn = () => {
+    push("/sign-in", { continue: pathname });
+  };
+  const signOut = async () => {
+    await firebaseAuth().signOut();
+    onClose();
+  };
+
+  return user ? (
+    <>
+      <div className={styles.accountHeader}>
+        <Avatar />
+        <span>{user.displayName}</span>
+      </div>
+      <div className={styles.promoteSignIn}>
+        <Button type="outline" label="サインアウト" onClick={signOut} />
+      </div>
+    </>
+  ) : (
+    <>
+      <div className={styles.accountHeader}>
+        <Avatar />
+        <span>サインインしていません</span>
+      </div>
+      <div className={styles.promoteSignIn}>
+        <Button type="outline" label="サインイン" onClick={singIn} />
+      </div>
+    </>
+  );
+};
 
 type Props = {
   open: boolean;
   onClose: () => void;
 };
 export const AccountDialog: FC<Props> = ({ open, onClose }) => {
-  const { push } = useHistory();
-  const { pathname } = useLocation();
-
-  const singIn = () => push("/sign-in", { continue: pathname });
+  const userLoadable = useUser();
 
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogContent>
-        <div className={styles.accountHeader}>
-          <Avatar />
-          <span>サインインしていません</span>
-        </div>
-        <div className={styles.promoteSignIn}>
-          <Button type="outline" label="サインイン" onClick={singIn} />
-        </div>
+        {userLoadable.state === "hasValue" && (
+          <AccountHeader user={userLoadable.contents} onClose={onClose} />
+        )}
       </DialogContent>
     </Dialog>
   );
