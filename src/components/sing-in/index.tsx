8da3e95fd4be75cbeaaf1687fbc1f42cbb168c2d
Cano <contact.sasasaky@gmail.com>
2021-02-18 21:06:04 +0900

    ログイン済みの場合リダイレクト

--- src/components/sing-in/index.tsx
+++ src/components/sing-in/index.tsx
@@ -1,14 +1,32 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useHistory, useLocation } from "react-router";
 import { createProvider, firebaseAuth } from "../../core/firebase/auth";
+import { useUser } from "../../core/firebase/auth/hooks";
 import { ProviderId, ProviderIdValue } from "../../core/firebase/auth/types";
 import { SignInButton } from "./signin-button";
 import * as styles from "./styles";
 
+type LocationState =
+  | {
+      continue?: string;
+    }
+  | undefined;
+
 export const SignIn: FC = () => {
+  const { replace } = useHistory();
+  const { state } = useLocation<LocationState>();
+  const userLoadable = useUser();
+
   const signIn = (providerId: ProviderIdValue) => {
     const provider = createProvider(providerId);
     firebaseAuth().signInWithRedirect(provider);
   };
+
+  useEffect(() => {
+    if (userLoadable.state === "hasValue" && userLoadable.contents !== null) {
+      replace(state?.continue ?? "/");
+    }
+  });
 
   return (
     <div className={styles.wrapper}>
