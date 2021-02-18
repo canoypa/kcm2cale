52ba7ff16ae2dae0212b1410352bf1b44f4b13aa
Cano <contact.sasasaky@gmail.com>
2021-02-18 21:02:52 +0900

    add sign-in process

--- src/components/sing-in/index.tsx
+++ src/components/sing-in/index.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
+import { createProvider, firebaseAuth } from "../../core/firebase/auth";
 import { ProviderId, ProviderIdValue } from "../../core/firebase/auth/types";
 import { SignInButton } from "./signin-button";
 import * as styles from "./styles";
 
 export const SignIn: FC = () => {
-  const signIn = (provider: ProviderIdValue) => {};
+  const signIn = (providerId: ProviderIdValue) => {
+    const provider = createProvider(providerId);
+    firebaseAuth().signInWithRedirect(provider);
+  };
 
   return (
     <div className={styles.wrapper}>
