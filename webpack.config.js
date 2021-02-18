4123e1fe23bf3bbb7ad326d84a1a63d34231983e
Cano <contact.sasasaky@gmail.com>
2021-02-18 18:37:51 +0900

    add firebase config env var

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 
 const config = (env) => {
   const isProd = env.mode === "prod";
+
+  dotenv.config({
+    path: resolve(`./.env/firebase-config${!isProd && "-dev"}.env`),
+  });
 
   return {
     mode: isProd ? "production" : "development",
@@ -29,6 +35,17 @@ const config = (env) => {
     resolve: { extensions: [".js", ".ts", ".tsx"] },
 
     plugins: [
+      new webpack.DefinePlugin({
+        __FIREBASE_CONFIG__: JSON.stringify({
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          projectId: process.env.PROJECT_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGING_SENDER_ID,
+          appId: process.env.APP_ID,
+          measurementId: process.env.MEASUREMENT_ID,
+        }),
+      }),
       new HtmlWebpackPlugin({
         template: resolve("src/index.html"),
         inject: false,
