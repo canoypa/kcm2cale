3d78e805d24e40ce1a695711b19fb909548b0bf7
Cano <contact.sasasaky@gmail.com>
2021-02-18 21:01:50 +0900

    make createProvider

--- src/core/firebase/auth/index.ts
+++ src/core/firebase/auth/index.ts
@@ -1,4 +1,11 @@
 import "firebase/auth";
 import { firebaseApp } from "../app";
+import { ProviderIdValue } from "./types";
 
 export const firebaseAuth = firebaseApp.auth;
+
+export const createProvider = (providerId: ProviderIdValue) => {
+  if (providerId === "google") return new firebaseAuth.GoogleAuthProvider();
+  if (providerId === "twitter") return new firebaseAuth.TwitterAuthProvider();
+  throw new Error("Error: 不明なプロバイダ");
+};
