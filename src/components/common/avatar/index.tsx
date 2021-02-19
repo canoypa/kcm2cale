608b9aea51e347ab1b34714249566274fbf40adf
Cano <contact.sasasaky@gmail.com>
2021-02-19 13:41:47 +0900

    update Avatar
    firebase.User を受け取り
    photoURL が存在しない場合 IconAccount を表示

--- src/components/common/avatar/index.tsx
+++ src/components/common/avatar/index.tsx
@@ -1,16 +1,13 @@
+import firebase from "firebase/app";
 import { FC } from "react";
 import { IconAccount } from "../icons";
 
-type DummyUser = {
-  photoURL: string;
-};
-
 type Props = {
-  user?: DummyUser;
+  user: firebase.User | null;
   size?: number;
 };
 export const Avatar: FC<Props> = ({ user, size = 32 }) =>
-  user ? (
+  user?.photoURL ? (
     <img src={user.photoURL} width={size} height={size} />
   ) : (
     <IconAccount size={size} />
