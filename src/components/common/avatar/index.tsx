24f230d746c06d1bfa77dd3adde5fb664fdea357
Cano <contact.sasasaky@gmail.com>
2021-02-19 13:44:09 +0900

    角丸を追加

--- src/components/common/avatar/index.tsx
+++ src/components/common/avatar/index.tsx
@@ -1,14 +1,18 @@
 import firebase from "firebase/app";
 import { FC } from "react";
 import { IconAccount } from "../icons";
+import * as styles from "./styles";
 
 type Props = {
   user: firebase.User | null;
   size?: number;
 };
-export const Avatar: FC<Props> = ({ user, size = 32 }) =>
-  user?.photoURL ? (
-    <img src={user.photoURL} width={size} height={size} />
-  ) : (
-    <IconAccount size={size} />
-  );
+export const Avatar: FC<Props> = ({ user, size = 32 }) => (
+  <div className={styles.container}>
+    {user?.photoURL ? (
+      <img src={user.photoURL} width={size} height={size} />
+    ) : (
+      <IconAccount size={size} />
+    )}
+  </div>
+);
---
+++ src/components/common/avatar/styles.ts
@@ -0,0 +1,8 @@
+import { css } from "@emotion/css";
+
+export const container = css({
+  width: 32,
+  height: 32,
+  borderRadius: 16,
+  overflow: "hidden",
+});
