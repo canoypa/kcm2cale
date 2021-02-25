5d7b6e5c21948851bf01ad737155cc945fa5ae7b
Cano <contact.sasasaky@gmail.com>
2021-02-25 11:02:56 +0900

    make about page (zatsu)

---
+++ src/components/about/styles.ts
@@ -0,0 +1,21 @@
+import { css } from "@emotion/css";
+
+export const aboutContainer = css({
+  padding: 24,
+
+  "*": {
+    color: "rgba(0, 0, 0, 0.87)",
+  },
+
+  "h1, h2, h3, h4, h5, h6": {
+    lineHeight: "2em",
+  },
+
+  "h1, h2": {
+    borderBottom: "1px solid #e0e0e0",
+  },
+
+  "section:not(:last-child)": {
+    marginBottom: "2em",
+  },
+});
