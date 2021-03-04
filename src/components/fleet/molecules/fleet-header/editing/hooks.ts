812e458f493089b4ecfc0383f30b587f016c295b
Cano <contact.sasasaky@gmail.com>
2021-03-04 14:39:32 +0900

    make useCountValid

--- src/components/fleet/molecules/fleet-header/editing/hooks.ts
+++ src/components/fleet/molecules/fleet-header/editing/hooks.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useRecoilState } from "recoil";
 import {
   FleetDescriptionState,
@@ -32,4 +32,39 @@ export const useEditFleetInfo = () => {
 
     submit,
   };
+};
+
+type Valid = {
+  error: boolean;
+  countText: string;
+};
+
+/** 文字数をカウント */
+const countCharacter = (text: string): number => [...text].length;
+
+/**
+ * 指定の文字数以下かどうかとカウンタテキストを返す
+ *
+ * @param text カウント対象のテキスト
+ * @param count 文字数
+ */
+const getValidCharCount = (text: string, count: number): Valid => {
+  const textCount = countCharacter(text);
+  const error = textCount > count;
+  const countText = `${textCount}/${count}`;
+
+  return {
+    error,
+    countText,
+  };
+};
+
+/**
+ * 指定の文字数以下かどうかとカウンタテキストを返す
+ *
+ * @param text カウント対象のテキスト
+ * @param count 文字数
+ */
+export const useCountValid = (text: string, count: number) => {
+  return useMemo(() => getValidCharCount(text, count), [text, count]);
 };
