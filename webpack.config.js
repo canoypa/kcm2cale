b5159392f5875480ae3ceb74a9f53df7bb11d812
Cano <contact.sasasaky@gmail.com>
2021-03-09 11:48:06 +0900

    firebase config を単一の環境変数で定義

--- webpack.config.js
+++ webpack.config.js
@@ -45,15 +45,7 @@ const config = (env) => {
         __APP_NAME__: JSON.stringify(variable.appName),
         __APP_VERSION__: JSON.stringify(appVersion),
 
-        __FIREBASE_CONFIG__: JSON.stringify({
-          apiKey: process.env.API_KEY,
-          authDomain: process.env.AUTH_DOMAIN,
-          projectId: process.env.PROJECT_ID,
-          storageBucket: process.env.STORAGE_BUCKET,
-          messagingSenderId: process.env.MESSAGING_SENDER_ID,
-          appId: process.env.APP_ID,
-          measurementId: process.env.MEASUREMENT_ID,
-        }),
+        __FIREBASE_CONFIG__: process.env.FIREBASE_CONFIG,
       }),
       new HtmlWebpackPlugin({
         template: resolve("src/index.html"),
