c60c479cc8fa1fa3423aa2b79a710bb001dd095e
Cano <contact.sasasaky@gmail.com>
2021-04-09 11:52:43 +0900

    def in app fleetdata type

---
+++ src/core/fleet-data/types.ts
@@ -0,0 +1,30 @@
+import { FleetType } from "../../store/organize/info";
+
+export interface FleetData {
+  /** Fleet Id */
+  id: string;
+
+  /** Fleet Type */
+  type: FleetType;
+  /** Fleet Title */
+  title: string;
+  /** Fleet description */
+  description: string;
+
+  /** Ships */
+  ships: Array<{
+    /** Fleet No */
+    fleetNo: number;
+    /** Turn No */
+    turnNo: number;
+    /** Ship No */
+    no: string;
+    /** Equipments */
+    equipments: Array<{
+      /** Slot No */
+      slotNo: number;
+      /** Equipment No */
+      no: number;
+    }>;
+  }>;
+}
