6da24716e470c1dfcb2e7fa8614aa3ebdab4b2dc
Cano <contact.sasasaky@gmail.com>
2021-04-12 11:50:48 +0900

    isNewFleet によって保存方法を分岐

--- src/core/persistence/save-to-local.ts
+++ src/core/persistence/save-to-local.ts
@@ -1,10 +1,12 @@
 import { Snapshot } from "recoil";
-import { FleetDateState } from "../../store/organize/info";
+import { FleetDateState, IsNewFleetState } from "../../store/organize/info";
 import { createFleetStates } from "./create-fleet-states";
 import { LocalDatabase } from "./local-database";
 import { encodeLocalFleetData } from "./local-fleet-data";
 
 export const saveToLocal = async (_snapshot: Snapshot) => {
+  const isNewFleet = _snapshot.getPromise(IsNewFleetState);
+
   // Fixme
   // eslint-disable-next-line array-callback-return
   const snapshot = _snapshot.map(({ set }) => {
@@ -14,8 +16,15 @@ export const saveToLocal = async (_snapshot: Snapshot) => {
   });
   const fleetStates = await createFleetStates(snapshot);
 
-  await LocalDatabase.setFleet(
-    fleetStates.fleetId,
-    encodeLocalFleetData(fleetStates)
-  );
+  if (isNewFleet) {
+    await LocalDatabase.setFleet(
+      fleetStates.fleetId,
+      encodeLocalFleetData(fleetStates)
+    );
+  } else {
+    await LocalDatabase.updateFleet(
+      fleetStates.fleetId,
+      encodeLocalFleetData(fleetStates)
+    );
+  }
 };
