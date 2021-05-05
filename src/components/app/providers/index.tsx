934f5ee68387575722184a683fe5df4fc0c21b09
Cano <contact.sasasaky@gmail.com>
2021-05-05 14:16:34 +0900

    global styles

--- src/components/app/providers/index.tsx
+++ src/components/app/providers/index.tsx
@@ -1,13 +1,16 @@
 import { FC, StrictMode } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { GlobalStyles } from "./global-styles";
 import { ThemeProvider } from "./theme-provider";
 
 export const RootProviders: FC = ({ children }) => (
   <StrictMode>
     <RecoilRoot>
       <Router>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <GlobalStyles>{children}</GlobalStyles>
+        </ThemeProvider>
       </Router>
     </RecoilRoot>
   </StrictMode>
