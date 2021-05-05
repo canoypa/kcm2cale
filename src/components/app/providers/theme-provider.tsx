65d087763a230f79e9202d41484ce0285b4a2d65
Cano <contact.sasasaky@gmail.com>
2021-05-05 11:11:30 +0900

    ThemeState 値に応じて theme を変更

--- src/components/app/providers/theme-provider.tsx
+++ src/components/app/providers/theme-provider.tsx
@@ -1,14 +1,14 @@
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core";
 import { ReactNode, useMemo, VFC } from "react";
+import { useRecoilValue } from "recoil";
 import { createTheme } from "../../../core/theme";
-
-const DUMMY_PALETTE_MODE = "light";
+import { ThemeState } from "../../../store/general/theme";
 
 type Props = {
   children: ReactNode;
 };
 export const ThemeProvider: VFC<Props> = ({ children }) => {
-  const mode = DUMMY_PALETTE_MODE;
+  const mode = useRecoilValue(ThemeState);
   const theme = useMemo(() => createTheme(mode), [mode]);
 
   return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
