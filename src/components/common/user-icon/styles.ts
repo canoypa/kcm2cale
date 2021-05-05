c1d8265966fe70b490fc366b3134be86a4cb9003
Cano <contact.sasasaky@gmail.com>
2021-05-05 16:23:50 +0900

    MainAppBar

--- src/components/common/user-icon/styles.ts
+++ src/components/common/user-icon/styles.ts
@@ -4,13 +4,10 @@ export const container = css({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  width: 48,
-  height: 48,
 });
 
 export const icon = css({
-  width: 32,
-  height: 32,
+  display: "inline-flex",
   borderRadius: 16,
   overflow: "hidden",
 });
