e5a773eef57e5702a93a2f9f8254e7018aaabc3b
Cano <contact.sasasaky@gmail.com>
2021-05-05 14:17:34 +0900

    fab

--- src/components/home/create-new-fleet/styles.ts
+++ src/components/home/create-new-fleet/styles.ts
@@ -1,16 +1,24 @@
-import { css } from "@emotion/css";
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
 
-export const container = css({
-  position: "fixed",
-  left: 0,
-  bottom: 0,
-  display: "flex",
-  justifyContent: "center",
-  padding: 24,
-  width: "100%",
-  boxSizing: "border-box",
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      position: "fixed",
+      left: 0,
+      bottom: 0,
+      display: "flex",
+      justifyContent: "center",
+      padding: 24,
+      width: "100%",
+      boxSizing: "border-box",
 
-  ["@media (max-width: 719px)"]: {
-    padding: 16,
-  },
-});
+      ["@media (max-width: 719px)"]: {
+        padding: 16,
+      },
+    },
+
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  })
+);
