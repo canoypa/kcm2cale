8e0b23c7d731861ba90547b155498fb9de9873fd
Cano <contact.sasasaky@gmail.com>
2021-05-09 22:06:16 +0900

    rename clamp to count

--- src/components/common/clamp/index.tsx
+++ src/components/common/clamp/index.tsx
@@ -2,10 +2,10 @@ import { FC } from "react";
 import { useLineClampStyles } from "./styles";
 
 type Props = {
-  clamp?: number;
+  count?: number;
   children: string;
 };
-export const LineClamp: FC<Props> = ({ clamp = 4, children }) => {
-  const classes = useLineClampStyles({ count: clamp });
+export const LineClamp: FC<Props> = ({ count = 4, children }) => {
+  const classes = useLineClampStyles({ count });
   return <span className={classes.root}>{children}</span>;
 };
