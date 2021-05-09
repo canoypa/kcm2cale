aeee6ba6453aee79e12c3df28a041de097909155
Cano <contact.sasasaky@gmail.com>
2021-05-09 22:07:29 +0900

    change count prop to required

--- src/components/common/clamp/index.tsx
+++ src/components/common/clamp/index.tsx
@@ -2,10 +2,10 @@ import { FC } from "react";
 import { useLineClampStyles } from "./styles";
 
 type Props = {
-  count?: number;
+  count: number;
   children: string;
 };
-export const LineClamp: FC<Props> = ({ count = 4, children }) => {
+export const LineClamp: FC<Props> = ({ count, children }) => {
   const classes = useLineClampStyles({ count });
   return <span className={classes.root}>{children}</span>;
 };
