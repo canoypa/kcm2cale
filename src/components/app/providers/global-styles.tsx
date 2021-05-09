c365e9ea944969db9111a6deec0e235ed62319af
Cano <contact.sasasaky@gmail.com>
2021-05-10 08:26:18 +0900

    add CssBaseline

--- src/components/app/providers/global-styles.tsx
+++ src/components/app/providers/global-styles.tsx
@@ -1,14 +1,10 @@
-import { makeStyles, Theme } from "@material-ui/core";
+import { CssBaseline, makeStyles, Theme } from "@material-ui/core";
 import { ReactNode, VFC } from "react";
 
 const useStyles = makeStyles((theme: Theme) => ({
   "@global": {
     body: {
-      margin: 0,
-      padding: 0,
       WebkitTapHighlightColor: "transparent",
-      backgroundColor: theme.palette.background.default,
-      color: theme.palette.text.primary,
     },
   },
 }));
@@ -19,5 +15,10 @@ type Props = {
 export const GlobalStyles: VFC<Props> = ({ children }) => {
   useStyles();
 
-  return <>{children}</>;
+  return (
+    <>
+      <CssBaseline />
+      {children}
+    </>
+  );
 };
