14c9a330277314cffe9655a3cd9dfffaf3442464
Cano <contact.sasasaky@gmail.com>
2021-05-10 08:49:57 +0900

    override styles

--- src/components/app/providers/global-styles.tsx
+++ src/components/app/providers/global-styles.tsx
@@ -1,20 +1,22 @@
-import { CssBaseline, makeStyles, Theme } from "@material-ui/core";
+import { CssBaseline as MuiCssBaseline, withStyles } from "@material-ui/core";
 import { ReactNode, VFC } from "react";
 
-const useStyles = makeStyles((theme: Theme) => ({
+const CssBaseline = withStyles((theme) => ({
   "@global": {
-    body: {
+    html: {
+      colorScheme: "dark light",
       WebkitTapHighlightColor: "transparent",
     },
+    body: {
+      ...theme.typography.body1,
+    },
   },
-}));
+}))(MuiCssBaseline);
 
 type Props = {
   children: ReactNode;
 };
 export const GlobalStyles: VFC<Props> = ({ children }) => {
-  useStyles();
-
   return (
     <>
       <CssBaseline />
