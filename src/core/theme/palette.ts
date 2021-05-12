8df6b69461f5d3d2e6c3f51bd5c4454aca7e1496
Cano <contact.sasasaky@gmail.com>
2021-05-12 17:21:35 +0900

    adjust

--- src/core/theme/palette.ts
+++ src/core/theme/palette.ts
@@ -2,35 +2,27 @@ import { PaletteOptions } from "@material-ui/core/styles/createPalette";
 
 const PALETTE_BASE: PaletteOptions = {
   primary: {
-    main: "#5c6bc0",
-    light: "#5c6bc0",
-    dark: "#5c6bc0",
+    main: "#3f51b5",
     contrastText: "rgba(255, 255, 255, 0.87)",
   },
   info: {
     main: "#03a9f4",
-    light: "#03a9f4",
-    dark: "#03a9f4",
     contrastText: "rgba(255, 255, 255, 0.87)",
   },
   warning: {
     main: "#ffc107",
-    light: "#ffc107",
-    dark: "#ffc107",
     contrastText: "rgba(255, 255, 255, 0.87)",
   },
   error: {
     main: "#f44336",
-    light: "#f44336",
-    dark: "#f44336",
     contrastText: "rgba(255, 255, 255, 0.87)",
   },
   success: {
     main: "#8bc34a",
-    light: "#8bc34a",
-    dark: "#8bc34a",
     contrastText: "rgba(255, 255, 255, 0.87)",
   },
+
+  tonalOffset: 0.1,
 };
 
 export const PALETTE_LIGHT: PaletteOptions = {
@@ -43,9 +35,6 @@ export const PALETTE_LIGHT: PaletteOptions = {
     secondary: "rgba(0, 0, 0, 0.6)",
     disabled: "rgba(0, 0, 0, 0.38)",
   },
-  // Todo: なんの値かわかったら追加して
-  //   action: {
-  //   },
   background: {
     default: "#ffffff",
   },
@@ -62,9 +51,6 @@ export const PALETTE_DARK: PaletteOptions = {
     secondary: "rgba(255, 255, 255, 0.6)",
     disabled: "rgba(255, 255, 255, 0.38)",
   },
-  // Todo: なんの値かわかったら追加して
-  //   action: {
-  //   },
   background: {
     default: "#121212",
   },
