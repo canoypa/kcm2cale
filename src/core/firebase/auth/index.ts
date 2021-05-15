215825668135d916ce85895bd76b800dbea17564
Cano <contact.sasasaky@gmail.com>
2021-05-15 12:16:42 +0900

    use auth emulator

--- src/core/firebase/auth/index.ts
+++ src/core/firebase/auth/index.ts
@@ -4,6 +4,10 @@ import { ProviderIdValue } from "./types";
 
 export const firebaseAuth = firebaseApp.auth;
 
+if (!__IS_PRODUCTION__) {
+  firebaseAuth().useEmulator("https://localhost:9099");
+}
+
 export const createProvider = (providerId: ProviderIdValue) => {
   if (providerId === "google") return new firebaseAuth.GoogleAuthProvider();
   if (providerId === "twitter") return new firebaseAuth.TwitterAuthProvider();
