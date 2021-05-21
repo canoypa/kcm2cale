50091895e57cd0d6994a952d4356993e44e5083d
Cano <contact.sasasaky@gmail.com>
2021-05-21 21:57:17 +0900

    states and hooks

--- src/core/search/fleet/index.ts
+++ src/core/search/fleet/index.ts
@@ -1,4 +1,12 @@
 import Fuse from "fuse.js";
+import {
+  atom,
+  selector,
+  SetterOrUpdater,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 import { LocalDatabase } from "../../persistence/local-database";
 import { LocalFleetData_v1 } from "../../persistence/types";
 import { SearchFleetRequest } from "./types";
@@ -10,7 +18,7 @@ const dateSortFn = (a: LocalFleetData_v1, b: LocalFleetData_v1): number => {
 };
 
 // TODO: テスト、書く
-export default class FleetSearch {
+class FleetSearch {
   public static search = async (
     request: SearchFleetRequest
   ): Promise<LocalFleetData_v1[]> => {
@@ -42,3 +50,69 @@ export default class FleetSearch {
     return result;
   };
 }
+
+/**
+ * FleetListState 更新用
+ */
+const FleetListRequestIdState = atom({
+  key: "FleetListRequestId",
+  default: 0,
+});
+
+/**
+ * FleetList 検索クエリ
+ */
+const SearchFleetQueryState = atom({
+  key: "SearchFleetQuery",
+  default: "",
+});
+
+/**
+ * 保存された編成が存在するか
+ */
+const IsExistFleetState = selector({
+  key: "IsExistFleet",
+  get: async ({ get }) => {
+    // refresh query
+    get(FleetListRequestIdState);
+
+    return Boolean(await LocalDatabase.fleetLength());
+  },
+});
+
+/**
+ * FleetList
+ */
+const FleetListState = selector({
+  key: "FleetList",
+  get: async ({ get }) => {
+    // refresh query
+    get(FleetListRequestIdState);
+
+    const request = {
+      q: get(SearchFleetQueryState),
+    };
+    return await FleetSearch.search(request);
+  },
+});
+
+type useRefreshFleetList = () => () => void;
+export const useRefreshFleetList: useRefreshFleetList = () => {
+  const setRequestId = useSetRecoilState(FleetListRequestIdState);
+  return () => setRequestId((id) => id + 1);
+};
+
+type useSearchFleetQuery = () => [string, SetterOrUpdater<string>];
+export const useSearchFleetQuery: useSearchFleetQuery = () => {
+  return useRecoilState(SearchFleetQueryState);
+};
+
+type useIsExistFleet = () => boolean;
+export const useIsExistFleet: useIsExistFleet = () => {
+  return useRecoilValue(IsExistFleetState);
+};
+
+type useFleetList = () => LocalFleetData_v1[];
+export const useFleetList: useFleetList = () => {
+  return useRecoilValue(FleetListState);
+};
