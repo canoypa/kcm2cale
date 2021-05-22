a89b417cf7627e9c9b736278ec117926ea9910c0
Cano <contact.sasasaky@gmail.com>
2021-05-22 09:47:03 +0900

    ページ下部に表示される警告メッセージを表示しない

--- src/core/firebase/auth/index.ts
+++ src/core/firebase/auth/index.ts
@@ -5,7 +5,12 @@ import { ProviderIdValue } from "./types";
 export const firebaseAuth = firebaseApp.auth;
 
 if (!__IS_PRODUCTION__) {
-  firebaseAuth().useEmulator("http://localhost:9099");
+  // @ts-expect-error
+  // 実際には useEmulator は第2引数に設定を受け取る
+  firebaseAuth().useEmulator("http://localhost:9099", {
+    // ページ下部に表示されるエミュレータ使用警告メッセージを表示しない
+    disableWarnings: true,
+  });
 }
 
 export const createProvider = (providerId: ProviderIdValue) => {
