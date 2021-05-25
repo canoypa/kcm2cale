7fd982e4a5aa9bee0cab8db77ac0837ebb39d1e4
Cano <contact.sasasaky@gmail.com>
2021-05-25 17:15:58 +0900

    sb upgrade

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,4 +10,10 @@ export const parameters = {
   },
 
   actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
 };
