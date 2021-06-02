732f599673bf132db2ada6f793621efa1d6edcf6
Cano <contact.sasasaky@gmail.com>
2021-06-02 17:32:03 +0900

    enable react strict mode

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { appName, appVersion } = require("./scripts/build/variable.js");
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
+  // React StrictMode
+  reactStrictMode: true,
+
   env: {
     APP_NAME: appName,
     APP_VERSION: appVersion,
