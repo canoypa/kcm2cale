ba6e781c091942b9c95ea624e5cdf4c24f633fc0
Cano <contact.sasasaky@gmail.com>
2021-06-07 18:22:15 +0900

    move states to index

--- src/store/organize/equipments/index.ts
+++ src/store/organize/equipments/index.ts
@@ -1,2 +1,16 @@
-export * from "./state";
+import { atom } from "recoil";
+import { DeployedFleetEquipment, EquipmentsStateValue } from "./types";
+
 export * from "./types";
+
+export type RiggingState = DeployedFleetEquipment[];
+export const RiggingState = atom<RiggingState>({
+  key: "RiggingState",
+  default: [],
+});
+
+export type EquipmentsState = EquipmentsStateValue[];
+export const EquipmentsState = atom<EquipmentsState>({
+  key: "EquipmentsState",
+  default: [],
+});
--- src/store/organize/ships/index.ts
+++ src/store/organize/ships/index.ts
@@ -1,2 +1,16 @@
-export * from "./state";
+import { atom } from "recoil";
+import { DeployedFleetShip, ShipStateValue } from "./types";
+
 export * from "./types";
+
+export type FleetState = DeployedFleetShip[];
+export const FleetState = atom<FleetState>({
+  key: "FleetState",
+  default: [],
+});
+
+export type ShipsState = ShipStateValue[];
+export const ShipsState = atom<ShipsState>({
+  key: "Ships",
+  default: [],
+});
