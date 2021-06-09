b5bfc6cd2f8d2152685cee3366494f664ce634a6
Cano <contact.sasasaky@gmail.com>
2021-06-09 13:33:54 +0900

    preload auth

--- src/core/firebase/app/index.ts
+++ src/core/firebase/app/index.ts
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import { preloadAuth } from "reactfire";
 
 export { default as firebase } from "firebase/app";
 
@@ -10,4 +11,17 @@ export const firebaseApp = firebase.initializeApp({
   messagingSenderId: "719133698825",
   appId: "1:719133698825:web:5bd598038750d7095481d8",
   measurementId: "G-VH9GB7XRVY",
+});
+
+preloadAuth({
+  firebaseApp,
+  setup: (auth) => {
+    if (!__IS_PRODUCTION__) {
+      // @ts-expect-error: 型定義にないオプション引数
+      auth().useEmulator("http://localhost:9099", {
+        // ページ下部に表示されるエミュレータ使用警告メッセージを表示しない
+        disableWarnings: true,
+      });
+    }
+  },
 });
--- src/core/firebase/auth/index.ts
+++ src/core/firebase/auth/index.ts
@@ -4,14 +4,6 @@ import { ProviderIdValue } from "./types";
 
 export const firebaseAuth = firebase.auth;
 
-if (!__IS_PRODUCTION__) {
-  // @ts-expect-error: 型定義にないオプション引数
-  firebaseAuth().useEmulator("http://localhost:9099", {
-    // ページ下部に表示されるエミュレータ使用警告メッセージを表示しない
-    disableWarnings: true,
-  });
-}
-
 export const createProvider = (providerId: ProviderIdValue) => {
   if (providerId === "google") return new firebaseAuth.GoogleAuthProvider();
   if (providerId === "twitter") return new firebaseAuth.TwitterAuthProvider();
