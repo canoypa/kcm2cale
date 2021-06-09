2f941296ede6c75475d680fb6b0074af526dc2aa
Cano <contact.sasasaky@gmail.com>
2021-06-09 13:29:13 +0900

    add FirebaseAppProvider

--- src/components/app/providers/index.tsx
+++ src/components/app/providers/index.tsx
@@ -1,17 +1,21 @@
 import { FC, StrictMode } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { FirebaseAppProvider } from "reactfire";
 import { RecoilRoot } from "recoil";
+import { firebaseApp } from "../../../core/firebase/app";
 import { GlobalStyles } from "./global-styles";
 import { ThemeProvider } from "./theme-provider";
 
 export const RootProviders: FC = ({ children }) => (
   <StrictMode>
     <RecoilRoot>
-      <Router>
-        <ThemeProvider>
-          <GlobalStyles>{children}</GlobalStyles>
-        </ThemeProvider>
-      </Router>
+      <FirebaseAppProvider firebaseApp={firebaseApp} suspense>
+        <Router>
+          <ThemeProvider>
+            <GlobalStyles>{children}</GlobalStyles>
+          </ThemeProvider>
+        </Router>
+      </FirebaseAppProvider>
     </RecoilRoot>
   </StrictMode>
 );
