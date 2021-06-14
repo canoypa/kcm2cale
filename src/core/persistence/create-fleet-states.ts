031dc71717811c387efb4f921b8673d8e56457ba
Cano <contact.sasasaky@gmail.com>
2021-06-14 20:32:27 +0900

    createPartialFleetStates

--- src/core/persistence/create-fleet-states.ts
+++ src/core/persistence/create-fleet-states.ts
@@ -1,4 +1,4 @@
-import { Snapshot } from "recoil";
+import { RecoilValue, Snapshot } from "recoil";
 import { EquipmentsState, RiggingState } from "../../store/organize/equipments";
 import {
   FleetDescriptionState,
@@ -19,39 +19,71 @@ export type FleetStates = {
   equipments: EquipmentsState;
 };
 
-export const createFleetStates = async (
+export type PartialFleetStates = Pick<FleetStates, "fleetId"> &
+  Partial<Omit<FleetStates, "fleetId">>;
+
+// 更新検知対象 State
+const fleetStateAtoms = [
+  FleetIdState,
+  FleetNameState,
+  FleetDescriptionState,
+  FleetTypeState,
+  FleetState,
+  ShipsState,
+  RiggingState,
+  EquipmentsState,
+];
+
+const FleetStatesKeyMap = {
+  [FleetIdState.key]: "fleetId",
+  [FleetNameState.key]: "fleetName",
+  [FleetDescriptionState.key]: "fleetDescription",
+  [FleetTypeState.key]: "fleetType",
+  [FleetState.key]: "fleet",
+  [ShipsState.key]: "ships",
+  [RiggingState.key]: "rigging",
+  [EquipmentsState.key]: "equipments",
+};
+
+const gerLabeledPromise = async <T>(
+  atom: RecoilValue<T>,
+  snapshot: Snapshot
+): Promise<[label: string, value: T]> => {
+  return [FleetStatesKeyMap[atom.key], await snapshot.getPromise(atom)];
+};
+
+const getModifiedFleetStates = (snapshot: Snapshot) => {
+  return [...snapshot.getNodes_UNSTABLE({ isModified: true })].filter(
+    ({ key }) => {
+      return fleetStateAtoms.some((fsa) => key === fsa.key);
+    }
+  );
+};
+
+export const createAllFleetStates = async (
   snapshot: Snapshot
 ): Promise<FleetStates> => {
-  const states = await Promise.all([
-    snapshot.getPromise(FleetIdState),
-    snapshot.getPromise(FleetNameState),
-    snapshot.getPromise(FleetDescriptionState),
-    snapshot.getPromise(FleetTypeState),
-    snapshot.getPromise(FleetState),
-    snapshot.getPromise(ShipsState),
-    snapshot.getPromise(RiggingState),
-    snapshot.getPromise(EquipmentsState),
-  ]);
+  const fleetStates = Object.fromEntries(
+    await Promise.all(
+      (fleetStateAtoms as Array<RecoilValue<any>>).map((v) =>
+        gerLabeledPromise(v, snapshot)
+      )
+    )
+  ) as FleetStates;
 
-  const [
-    fleetId,
-    fleetName,
-    fleetDescription,
-    fleetType,
-    fleet,
-    ships,
-    rigging,
-    equipments,
-  ] = states;
+  return fleetStates;
+};
 
-  return {
-    fleetId,
-    fleetName,
-    fleetDescription,
-    fleetType,
-    fleet,
-    ships,
-    rigging,
-    equipments,
-  };
+export const createPartialFleetStates = async (
+  snapshot: Snapshot
+): Promise<PartialFleetStates> => {
+  const fleetStates = Object.fromEntries(
+    await Promise.all(
+      getModifiedFleetStates(snapshot).map((v) =>
+        gerLabeledPromise(v, snapshot)
+      )
+    )
+  ) as PartialFleetStates;
+
+  return fleetStates;
 };
--- src/core/persistence/save-to-local.ts
+++ src/core/persistence/save-to-local.ts
@@ -1,13 +1,13 @@
 import { Snapshot } from "recoil";
 import { IsNewFleetState } from "../../store/organize/info";
-import { createFleetStates } from "./create-fleet-states";
+import { createAllFleetStates } from "./create-fleet-states";
 import { LocalDatabase } from "./local-database";
 import { encodeLocalFleetData } from "./local-fleet-data";
 
 export const saveToLocal = async (snapshot: Snapshot) => {
   const isNewFleet = await snapshot.getPromise(IsNewFleetState);
 
-  const fleetStates = await createFleetStates(snapshot);
+  const fleetStates = await createAllFleetStates(snapshot);
 
   if (isNewFleet) {
     await LocalDatabase.setFleet(
