5e01ffa48df396afad38ee02b0ef540a85c907a0
Cano <contact.sasasaky@gmail.com>
2021-06-21 10:16:33 +0900

    change sign-in method

--- src/components/signin/signin-form/index.tsx
+++ src/components/signin/signin-form/index.tsx
@@ -1,20 +1,50 @@
 import { Box, Typography } from "@material-ui/core";
 import { FC } from "react";
-import { useAuth } from "reactfire";
+import { useAuth, useUser } from "reactfire";
 import { createProvider } from "../../../core/firebase/auth";
 import { ProviderId, ProviderIdValue } from "../../../core/firebase/auth/types";
+import { useDidMount } from "../../../util/hooks/lifecycle";
 import { SignInButton } from "../signin-button";
 import { useStyles } from "./styles";
 
 export const SignInForm: FC = () => {
   const auth = useAuth();
+  const { data: user } = useUser();
 
   const classes = useStyles();
 
   const signIn = (providerId: ProviderIdValue) => {
     const provider = createProvider(providerId);
-    auth.signInWithRedirect(provider);
+    user.linkWithRedirect(provider);
   };
+
+  useDidMount(() => {
+    auth
+      .getRedirectResult()
+      .then((result) => {
+        // 正常にリンク完了
+        console.log(result);
+      })
+      .catch(async (error) => {
+        // すでに作成済みのアカウントの場合はそちらでサインインする
+        if (error.code === "auth/credential-already-in-use") {
+          // Todo: 匿名認証時に作成された編成がある場合移行するか尋ねる？
+
+          // 現在のユーザを変数に逃がす
+          const prevUser = user;
+
+          // 認証情報でサインイン
+          await auth.signInWithCredential(error.credential);
+
+          // 匿名ユーザを削除
+          prevUser.delete();
+
+          return;
+        }
+
+        throw error;
+      });
+  });
 
   return (
     <div className={classes.root}>
