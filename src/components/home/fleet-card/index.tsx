7179bfcda35eda373572853ccc4b459b0a821621
Cano <contact.sasasaky@gmail.com>
2021-06-25 22:43:14 +0900

    fleet delete process

--- src/components/home/fleet-card/index.tsx
+++ src/components/home/fleet-card/index.tsx
@@ -9,13 +9,15 @@ import {
 import { MoreVert } from "@material-ui/icons";
 import { FC, MouseEvent, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { LocalDatabase } from "../../../core/persistence/local-database";
+import { useFirestore } from "reactfire";
 import { FireFleet } from "../../../models/fleet";
 import { LineClamp } from "../../common/clamp";
 import { useStyles } from "./styles";
 
 type Props = { fleetData: FireFleet };
 export const FleetCard: FC<Props> = ({ fleetData }) => {
+  const firestore = useFirestore();
+
   const [isMenuOpen, setMenuOpen] = useState(false);
 
   const menuAnchorEl = useRef<HTMLButtonElement>(null);
@@ -34,7 +36,27 @@ export const FleetCard: FC<Props> = ({ fleetData }) => {
   };
 
   const deleteFleet = async () => {
-    await LocalDatabase.deleteFleet(fleetData.id);
+    closeMenu();
+
+    // 各参照を取得
+    const fleeDocRef = firestore.doc(`fleets/${fleetData.id}`);
+    const shipsColRed = fleeDocRef.collection("ships");
+    const equipmentsColRed = fleeDocRef.collection("equipments");
+
+    // 艦,装備ドキュメントリストの取得
+    const { docs: shipDocs } = await shipsColRed.get();
+    const { docs: equipmentDocs } = await equipmentsColRed.get();
+
+    // バッチ処理開始
+    const batch = firestore.batch();
+
+    // 各ドキュメント削除
+    shipDocs.forEach((doc) => batch.delete(doc.ref));
+    equipmentDocs.forEach((doc) => batch.delete(doc.ref));
+    batch.delete(fleeDocRef);
+
+    // コミット
+    batch.commit();
   };
 
   return (
