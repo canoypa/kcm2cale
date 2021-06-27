211e3408e16bc733165b8acf71fba2f162e52269
Cano <contact.sasasaky@gmail.com>
2021-06-27 11:30:28 +0900

    作成者の場合変更を監視する

--- src/components/fleet/fleet-data-provider/index.tsx
+++ src/components/fleet/fleet-data-provider/index.tsx
@@ -1,6 +1,6 @@
-import React, { FC, ReactNode, useEffect, useState } from "react";
+import React, { FC, ReactNode, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router";
-import { useFirestore } from "reactfire";
+import { useFirestore, useUser } from "reactfire";
 import { FirestoreFleetConverter } from "../../../core/firestore-converter";
 import { FirestoreFleetEquipmentsConverter } from "../../../core/firestore-converter/equipments";
 import { FirestoreFleetShipsConverter } from "../../../core/firestore-converter/ships";
@@ -18,6 +18,7 @@ type Props = {
   children: ReactNode;
 };
 export const FleetDataProvider: FC<Props> = ({ children }) => {
+  const { data: user } = useUser();
   const firestore = useFirestore();
 
   // url から fleetId を取得
@@ -30,6 +31,22 @@ export const FleetDataProvider: FC<Props> = ({ children }) => {
     undefined
   );
 
+  // 作成者の場合のみ変更を受け取るようにするあれこれ
+  // next.js 移行できれいにしてくれ...
+  const isOwner = useRef<boolean | undefined>(undefined);
+  const fDocUnsubscribe = useRef<(() => void) | undefined>(undefined);
+  const sCollUnsubscribe = useRef<(() => void) | undefined>(undefined);
+  const eCollUnsubscribe = useRef<(() => void) | undefined>(undefined);
+
+  // データが揃った後、作成者でなければリスナを解除
+  useEffect(() => {
+    if (!isOwner.current && fleet && ships && equipments) {
+      fDocUnsubscribe.current?.();
+      sCollUnsubscribe.current?.();
+      eCollUnsubscribe.current?.();
+    }
+  }, [fleet, ships, equipments]);
+
   useEffect(() => {
     const fleetDocRef = firestore
       .doc(`fleets/${fleetId}`)
@@ -41,24 +58,23 @@ export const FleetDataProvider: FC<Props> = ({ children }) => {
       .collection("equipments")
       .withConverter(FirestoreFleetEquipmentsConverter);
 
-    Promise.all([
-      fleetDocRef.get(),
-      shipsCollRef.get(),
-      equipmentsCollRef.get(),
-    ]).then(([f, s, e]) => {
-      if (f.exists) {
-        setFleet(f.data());
-        setShips(s.docs.map((d) => d.data()));
-        setEquipments(e.docs.map((d) => d.data()));
+    fDocUnsubscribe.current = fleetDocRef.onSnapshot((f) => {
+      const data = f.data();
+      if (data) {
+        isOwner.current = data.owner === user.uid;
 
-        return;
+        setFleet(data);
       }
-
-      setFleet(null);
-      setShips(null);
-      setEquipments(null);
     });
-  }, [firestore, fleetId]);
+
+    sCollUnsubscribe.current = shipsCollRef.onSnapshot((s) => {
+      setShips(s.docs.map((d) => d.data()));
+    });
+
+    eCollUnsubscribe.current = equipmentsCollRef.onSnapshot((e) => {
+      setEquipments(e.docs.map((d) => d.data()));
+    });
+  }, [firestore, fleetId, user.uid]);
 
   return (
     <FleetContext.Provider value={fleet}>
