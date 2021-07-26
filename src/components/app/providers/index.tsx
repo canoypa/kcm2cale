40016340b8d796cb19dc5e64bee9813a828553f6
Cano <contact.sasasaky@gmail.com>
2021-07-26 20:34:52 +0900

    swr config

--- src/components/app/providers/index.tsx
+++ src/components/app/providers/index.tsx
@@ -5,24 +5,27 @@ import { RecoilRoot } from "recoil";
 import { firebaseApp } from "../../../core/firebase/app";
 import { AuthProvider } from "./auth-provider";
 import { GlobalStyles } from "./global-styles";
+import { SWRConfig } from "./swrConfig";
 import { ThemeProvider } from "./theme-provider";
 
 export const RootProviders: FC = ({ children }) => (
   <StrictMode>
     <RecoilRoot>
-      <FirebaseAppProvider firebaseApp={firebaseApp} suspense>
-        <Router>
-          <ThemeProvider>
-            <GlobalStyles>
-              {/* Fixme: AuthProvider によるサスペンド LCPが低下するため */}
-              <Suspense fallback={null}>
-                <AuthProvider />
-                {children}
-              </Suspense>
-            </GlobalStyles>
-          </ThemeProvider>
-        </Router>
-      </FirebaseAppProvider>
+      <SWRConfig>
+        <FirebaseAppProvider firebaseApp={firebaseApp} suspense>
+          <Router>
+            <ThemeProvider>
+              <GlobalStyles>
+                {/* Fixme: AuthProvider によるサスペンド LCPが低下するため */}
+                <Suspense fallback={null}>
+                  <AuthProvider />
+                  {children}
+                </Suspense>
+              </GlobalStyles>
+            </ThemeProvider>
+          </Router>
+        </FirebaseAppProvider>
+      </SWRConfig>
     </RecoilRoot>
   </StrictMode>
 );
