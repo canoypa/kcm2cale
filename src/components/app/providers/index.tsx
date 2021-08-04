5b2469faf465db0a6fc3ac85ae2cd96a3cab7316
Cano <contact.sasasaky@gmail.com>
2021-08-04 14:52:31 +0900

    remove FirebaseAppProvider

--- src/components/app/providers/index.tsx
+++ src/components/app/providers/index.tsx
@@ -1,8 +1,6 @@
 import { FC, StrictMode, Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { FirebaseAppProvider } from "reactfire";
 import { RecoilRoot } from "recoil";
-import { firebaseApp } from "../../../core/firebase/app";
 import { AuthProvider } from "./auth-provider";
 import { GlobalStyles } from "./global-styles";
 import { SWRConfig } from "./swrConfig";
@@ -12,19 +10,17 @@ export const RootProviders: FC = ({ children }) => (
   <StrictMode>
     <RecoilRoot>
       <SWRConfig>
-        <FirebaseAppProvider firebaseApp={firebaseApp} suspense>
-          <Router>
-            <ThemeProvider>
-              <GlobalStyles>
-                {/* Fixme: AuthProvider によるサスペンド LCPが低下するため */}
-                <Suspense fallback={null}>
-                  <AuthProvider />
-                  {children}
-                </Suspense>
-              </GlobalStyles>
-            </ThemeProvider>
-          </Router>
-        </FirebaseAppProvider>
+        <Router>
+          <ThemeProvider>
+            <GlobalStyles>
+              {/* Fixme: AuthProvider によるサスペンド LCPが低下するため */}
+              <Suspense fallback={null}>
+                <AuthProvider />
+                {children}
+              </Suspense>
+            </GlobalStyles>
+          </ThemeProvider>
+        </Router>
       </SWRConfig>
     </RecoilRoot>
   </StrictMode>
