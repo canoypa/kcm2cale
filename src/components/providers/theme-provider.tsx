de6b91b00566e077f523b82deda2bcdcbd417896
Cano <contact.sasasaky@gmail.com>
2021-08-18 11:12:08 +0900

    theme without recoil

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -1,15 +1,96 @@
-import { ThemeProvider as MuiThemeProvider } from "@material-ui/core";
-import { ReactNode, useMemo, VFC } from "react";
-import { useRecoilValue } from "recoil";
+import {
+  PaletteType,
+  ThemeProvider as MuiThemeProvider,
+} from "@material-ui/core";
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useMemo,
+  useState,
+  VFC,
+} from "react";
 import { createTheme } from "../../core/theme";
-import { PaletteModeState } from "../../store/general/theme";
+
+const IsDarkMatchMedia = "(prefers-color-scheme:dark)";
+
+type ThemeType = PaletteType | "system";
+
+type Theme = {
+  /** 全てのテーマ名 */
+  themes: string[];
+  /** 現在のテーマ名 */
+  theme: string;
+  /** "system" を含む保存されたテーマ設定 */
+  configuredTheme: string;
+  /** テーマを更新 */
+  // setTheme: (theme: string) => void;
+};
+const ThemeContext = createContext<Theme>({
+  themes: [],
+  theme: "",
+  configuredTheme: "",
+});
+
+/**
+ * 保存されたテーマ設定を取得して返す
+ * 現在は "system" のみ
+ */
+const getConfiguredTheme = (): ThemeType => {
+  // サーバサイドでは "system" で処理
+  if (typeof window === "undefined") return "system";
+
+  return "system";
+};
+
+/**
+ * 保存されたテーマ設定が "system" の場合、システム設定を解決して返す
+ */
+const getResolvedTheme = (configuredTheme: ThemeType): PaletteType => {
+  // 設定値が "system" 出ない場合そのまま返す
+  if (configuredTheme !== "system") return configuredTheme;
+
+  // サーバサイドでは "light" で処理
+  if (typeof window === "undefined") return "light";
+
+  const matchList = window.matchMedia(IsDarkMatchMedia);
+  return matchList.matches ? "dark" : "light";
+};
 
 type Props = {
   children: ReactNode;
 };
 export const ThemeProvider: VFC<Props> = ({ children }) => {
-  const mode = useRecoilValue(PaletteModeState);
-  const theme = useMemo(() => createTheme(mode), [mode]);
+  const [configuredTheme] = useState(getConfiguredTheme());
+  const [resolvedTheme, setResolvedTheme] = useState(
+    getResolvedTheme(configuredTheme)
+  );
 
-  return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
+  /** 設定値が "system" の場合、matchMedia の更新を検知してテーマを更新 */
+  useEffect(() => {
+    const handler = () => {
+      setResolvedTheme(getResolvedTheme(configuredTheme));
+    };
+
+    const matchMedia = window.matchMedia(IsDarkMatchMedia);
+    if (configuredTheme === "system") {
+      matchMedia.addEventListener("change", handler);
+    }
+
+    return () => matchMedia.removeEventListener("change", handler);
+  }, [configuredTheme]);
+
+  const themeData = useMemo(() => createTheme(resolvedTheme), [resolvedTheme]);
+
+  const themeValue: Theme = {
+    themes: ["light", "dark"],
+    theme: resolvedTheme,
+    configuredTheme: configuredTheme,
+  };
+
+  return (
+    <ThemeContext.Provider value={themeValue}>
+      <MuiThemeProvider theme={themeData}>{children}</MuiThemeProvider>
+    </ThemeContext.Provider>
+  );
 };
