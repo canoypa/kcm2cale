d6d5286648027522ae41d6179fd5e532c348300c
Cano <contact.sasasaky@gmail.com>
2021-08-20 16:32:34 +0900

    using dynamic import

--- src/components/fleet/SelectEquipmentDialog/index.tsx
+++ src/components/fleet/SelectEquipmentDialog/index.tsx
@@ -1,98 +1,13 @@
-import {
-  AppBar,
-  Dialog,
-  IconButton,
-  Toolbar,
-  Typography
-} from "@material-ui/core";
-import { NavigateBefore } from "@material-ui/icons";
+import { Dialog } from "@material-ui/core";
+import dynamic from "next/dynamic";
 import { FC, useCallback } from "react";
 import { useRecoilState } from "recoil";
 import {
-  equipmentGroupFilter,
-  EquipmentGroupMap,
-  EquipmentGroupValues
-} from "~/core/filters/equipment";
-import { EquipmentSearch } from "~/core/search/equipment";
-import { EquipmentData, ShipEquipment } from "~/models/equipment/types";
-import { OrganizeSelectSearchRenderer } from "../select-fleet-item/search-renderer";
-import {
   DefaultSelectEquipmentState,
-  SelectEquipmentAtom
+  SelectEquipmentAtom,
 } from "../store/select-equipment";
-import { useSearchQuery } from "./hooks/search-query";
-import { useSetEquipment } from "./hooks/set-equipment";
-import { SearchEquipmentsList } from "./search-equipments-list";
 
-/**
- * 原因不明のエラー
- * `Uncaught TypeError: Cannot read property 'getAttribute' of null`
- *
- * - keyDown で発生
- * - いずれかの要素に focus されているとエラーにならない
- */
-
-const isEquipmentGroupValue = (n: number): n is EquipmentGroupValues =>
-  n >= 0 && n <= 21;
-
-type SelectEquipmentProps = {
-  target: ShipEquipment;
-  onClose: () => void;
-};
-const SelectEquipment: FC<SelectEquipmentProps> = ({ target, onClose }) => {
-  const { query: searchQuery, setQuery, setTypes } = useSearchQuery();
-
-  const setEquipment = useSetEquipment();
-
-  const handler = {
-    filterChange: useCallback(
-      (filter: number | null) => {
-        if (filter === null) {
-          setTypes(null);
-        } else {
-          if (isEquipmentGroupValue(filter)) {
-            setTypes(EquipmentGroupMap[filter]);
-          }
-        }
-      },
-      [setTypes]
-    ),
-
-    onChangeQuery: useCallback((value: string) => setQuery(value), [setQuery]),
-
-    onSelect: useCallback(
-      (equipmentData: EquipmentData) => {
-        setEquipment(target, equipmentData);
-        onClose();
-      },
-      [onClose, setEquipment, target]
-    ),
-  };
-
-  const equipmentsList = EquipmentSearch.search(searchQuery);
-
-  return (
-    <>
-      <AppBar position="sticky" color="inherit">
-        <Toolbar>
-          <IconButton edge="start" onClick={onClose} aria-label="戻る">
-            <NavigateBefore />
-          </IconButton>
-          <Typography variant="h6">装備を選択</Typography>
-        </Toolbar>
-      </AppBar>
-      <SearchEquipmentsList
-        equipmentsList={equipmentsList}
-        onSelect={handler.onSelect}
-      />
-      <OrganizeSelectSearchRenderer
-        filterGroup={equipmentGroupFilter}
-        changeFilter={handler.filterChange}
-        changeQuery={handler.onChangeQuery}
-      />
-    </>
-  );
-};
+const SelectEquipment = dynamic(() => import("./SelectEquipment"));
 
 export const SelectEquipmentDialog: FC = () => {
   const [selectState, setSelectState] = useRecoilState(SelectEquipmentAtom);
--- src/components/fleet/SelectShipDialog/index.tsx
+++ src/components/fleet/SelectShipDialog/index.tsx
@@ -1,83 +1,10 @@
-import {
-  AppBar,
-  Dialog,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@material-ui/core";
-import { NavigateBefore } from "@material-ui/icons";
+import { Dialog } from "@material-ui/core";
+import dynamic from "next/dynamic";
 import { FC, useCallback } from "react";
 import { useRecoilState } from "recoil";
-import {
-  shipGroupFilter,
-  ShipSearchGroupMap,
-  ShipSearchGroupValues,
-} from "~/core/filters/ship";
-import { ShipSearch } from "~/core/search/ship";
-import { FleetShip, ShipData } from "~/models/ship";
-import { OrganizeSelectSearchRenderer } from "../select-fleet-item/search-renderer";
 import { DefaultSelectShipState, SelectShipAtom } from "../store/select-ship";
-import { useSearchQuery } from "./hooks/search-query";
-import { useSetShip } from "./hooks/set-ship";
-import { SearchShipsList } from "./search-ships-list";
 
-const isShipGroupValue = (n: number): n is ShipSearchGroupValues =>
-  n >= 0 && n <= 8;
-
-type SelectShipProps = {
-  target: FleetShip;
-  onClose: () => void;
-};
-const SelectShip: FC<SelectShipProps> = ({ target, onClose }) => {
-  const { query: searchQuery, setQuery, setTypes } = useSearchQuery();
-  const setShip = useSetShip();
-
-  const handler = {
-    filterChange: useCallback(
-      (filter: number | null) => {
-        if (filter === null) {
-          setTypes(null);
-        } else {
-          if (isShipGroupValue(filter)) {
-            setTypes(ShipSearchGroupMap[filter]);
-          }
-        }
-      },
-      [setTypes]
-    ),
-
-    onChangeQuery: useCallback((value: string) => setQuery(value), [setQuery]),
-
-    onSelect: useCallback(
-      (shipData: ShipData) => {
-        setShip(target, shipData);
-        onClose();
-      },
-      [onClose, setShip, target]
-    ),
-  };
-
-  const shipsList = ShipSearch.search(searchQuery);
-
-  return (
-    <>
-      <AppBar position="sticky" color="inherit">
-        <Toolbar>
-          <IconButton edge="start" onClick={onClose} aria-label="戻る">
-            <NavigateBefore />
-          </IconButton>
-          <Typography variant="h6">{"艦娘を選択"}</Typography>
-        </Toolbar>
-      </AppBar>
-      <SearchShipsList shipsList={shipsList} onSelect={handler.onSelect} />
-      <OrganizeSelectSearchRenderer
-        filterGroup={shipGroupFilter}
-        changeFilter={handler.filterChange}
-        changeQuery={handler.onChangeQuery}
-      />
-    </>
-  );
-};
+const SelectShip = dynamic(() => import("./SelectShip"));
 
 export const SelectShipDialog: FC = () => {
   const [selectState, setSelectState] = useRecoilState(SelectShipAtom);
