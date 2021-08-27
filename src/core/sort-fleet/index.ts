81bd44264ab98a037122d06bba3d02293ff23fe5
Cano <contact.sasasaky@gmail.com>
2021-08-27 10:57:14 +0900

    sortFleet firebase support

--- src/core/sort-fleet/index.ts
+++ src/core/sort-fleet/index.ts
@@ -1,40 +1,43 @@
-import { FleetPlace, FleetShip } from "../../models/ship";
+import { FleetNo, FleetShip, Ship, TurnNo } from "../../models/ship";
 
-type FleetState = FleetShip[];
+type SortRange = [from: TurnNo, to: TurnNo, increment: number];
+const getSortRange = (from: TurnNo, to: TurnNo): SortRange => {
+  if (from < to) return [from + 1, to, -1];
+  if (from > to) return [to, from - 1, 1];
 
+  // from === to
+  // 不正なソート呼び出し
+  throw new Error("Error");
+};
+
+/**
+ * ソート対象のソート後位置のリストを返す
+ */
 export const sortFleet = (
-  fleet: FleetState,
-  from: FleetPlace,
-  to: FleetPlace
-): FleetState => {
-  return fleet.map((fleetShip) => {
-    const fromTurnNo = from.turnNo;
-    const toTurnNo = to.turnNo;
+  fleet: FleetShip[],
+  fleetNo: FleetNo,
+  from: TurnNo,
+  to: TurnNo
+): Ship[] => {
+  // ソート対象範囲と移動量(向き)
+  const sortRange = getSortRange(from, to);
+  // ソート対象の Ship
+  const needSort: Ship[] = fleet.filter(
+    (v): v is Ship =>
+      // EmptyShip でない
+      v.no !== null &&
+      // ソート対象範囲内
+      // 同艦隊かつ
+      v.fleetNo === fleetNo &&
+      // ソート対象もしくは影響範囲
+      (v.turnNo === from ||
+        (sortRange[0] <= v.turnNo && v.turnNo <= sortRange[1]))
+  );
 
-    // ドラッグ対象の艦
-    const target = fleet.find(({ turnNo }) => turnNo === fromTurnNo);
-    if (!target) throw new Error("Error: ドラッグ対象のデータが見つからない");
-
-    // ドラッグの影響を受ける艦の範囲
-    // fromTurnNo は ドラッグ対象のため +1/-1
-    const start = fromTurnNo < toTurnNo ? fromTurnNo + 1 : toTurnNo;
-    const end = fromTurnNo < toTurnNo ? toTurnNo : fromTurnNo - 1;
-    // ドラッグの影響を受ける艦の移動向き
-    const move = fromTurnNo < toTurnNo ? -1 : 1;
-
-    // 同じ艦隊内のみ
-    if (fleetShip.fleetNo === from.fleetNo) {
-      // ドラッグ対象はドロップ位置へ移動
-      if (fleetShip.turnNo === target.turnNo) {
-        return { ...fleetShip, turnNo: toTurnNo };
-      }
-
-      // それ以外の from~to 間の艦を移動
-      if (start <= fleetShip.turnNo && fleetShip.turnNo <= end) {
-        return { ...fleetShip, turnNo: fleetShip.turnNo + move };
-      }
-    }
-
-    return fleetShip;
+  const sorted = needSort.map((ship) => {
+    if (ship.turnNo === from) return { ...ship, turnNo: to };
+    return { ...ship, turnNo: ship.turnNo + sortRange[2] };
   });
+
+  return sorted;
 };
--- src/core/sort-fleet/sort-fleet.test.ts
+++ src/core/sort-fleet/sort-fleet.test.ts
@@ -1,5 +1,5 @@
 import { sortFleet } from ".";
-import { FleetPlace, FleetShip } from "../../models/ship";
+import { FleetShip } from "../../models/ship";
 
 const fleetData: FleetShip[] = [
   { fleetNo: 0, turnNo: 0, id: "0", no: "0" },
@@ -9,28 +9,40 @@ const fleetData: FleetShip[] = [
 
 describe("SortFleet", () => {
   test("Single Move", () => {
-    const from: FleetPlace = { fleetNo: 0, turnNo: 0 };
-    const to: FleetPlace = { fleetNo: 0, turnNo: 1 };
-
     const expected = expect.arrayContaining([
       expect.objectContaining({ turnNo: 0, id: "1" }),
       expect.objectContaining({ turnNo: 1, id: "0" }),
-      expect.objectContaining({ turnNo: 2, id: "2" }),
     ]);
 
-    expect(sortFleet(fleetData, from, to)).toEqual(expected);
+    expect(sortFleet(fleetData, 0, 0, 1)).toEqual(expected);
+  });
+
+  test("Reversed Single Move", () => {
+    const expected = expect.arrayContaining([
+      expect.objectContaining({ turnNo: 1, id: "0" }),
+      expect.objectContaining({ turnNo: 0, id: "1" }),
+    ]);
+
+    expect(sortFleet(fleetData, 0, 1, 0)).toEqual(expected);
   });
 
   test("Multi Move", () => {
-    const from: FleetPlace = { fleetNo: 0, turnNo: 0 };
-    const to: FleetPlace = { fleetNo: 0, turnNo: 2 };
-
     const expected = expect.arrayContaining([
       expect.objectContaining({ turnNo: 0, id: "1" }),
       expect.objectContaining({ turnNo: 1, id: "2" }),
       expect.objectContaining({ turnNo: 2, id: "0" }),
     ]);
 
-    expect(sortFleet(fleetData, from, to)).toEqual(expected);
+    expect(sortFleet(fleetData, 0, 0, 2)).toEqual(expected);
+  });
+
+  test("Reversed Multi Move", () => {
+    const expected = expect.arrayContaining([
+      expect.objectContaining({ turnNo: 0, id: "2" }),
+      expect.objectContaining({ turnNo: 1, id: "0" }),
+      expect.objectContaining({ turnNo: 2, id: "1" }),
+    ]);
+
+    expect(sortFleet(fleetData, 0, 2, 0)).toEqual(expected);
   });
 });
