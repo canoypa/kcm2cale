61474f3bae3bf55122b138c3dad1d0213c6a314d
Cano <contact.sasasaky@gmail.com>
2021-09-02 13:20:59 +0900

    split

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,8 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { About } from "../../components/about/About";
+import { FAQ } from "../../components/about/FAQ";
 import { Header } from "../../components/about/Header";
-import { Intro } from "../../components/about/Intro";
 import { MarkdownStyle } from "../../components/about/MarkdownStyle";
 import { APP_NAME } from "../../core/env";
 
@@ -10,14 +9,13 @@ const HomePage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>About - {APP_NAME}</title>
+        <title>FAQ - {APP_NAME}</title>
       </Head>
 
       <main>
         <MarkdownStyle>
           <Header />
-          <Intro />
-          <About />
+          <FAQ />
         </MarkdownStyle>
       </main>
     </>
--- src/pages/privacy-and-terms/index.tsx
+++ src/pages/privacy-and-terms/index.tsx
@@ -1,23 +1,21 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { About } from "../../components/about/About";
 import { Header } from "../../components/about/Header";
-import { Intro } from "../../components/about/Intro";
 import { MarkdownStyle } from "../../components/about/MarkdownStyle";
+import { PrivacyAndTerms } from "../../components/about/PrivacyAndTerms";
 import { APP_NAME } from "../../core/env";
 
 const HomePage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>About - {APP_NAME}</title>
+        <title>Privacy And Terms - {APP_NAME}</title>
       </Head>
 
       <main>
         <MarkdownStyle>
           <Header />
-          <Intro />
-          <About />
+          <PrivacyAndTerms />
         </MarkdownStyle>
       </main>
     </>
