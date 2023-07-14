6a66095144a770fa2c8d99b49364d3a4611842a0
Cano <developer.sasazono@gmail.com>
2023-07-14 17:49:54 +0900

    useUser hook を削除

--- src/components/new-fleet/index.tsx
+++ src/components/new-fleet/index.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import { FC, useEffect } from "react";
-import { useUser } from "../../hooks/firebase/auth/useUser";
 import { useCreateNewFleet } from "./useCreateNewFleet";
 
 // 編成を新規作成してリダイレクト
@@ -8,16 +7,22 @@ export const NewFleet: FC = () => {
   const { replace } = useRouter();
   const createNewFleet = useCreateNewFleet();
 
-  const { data: user } = useUser();
+  // const { data: user } = useUser();
 
-  useEffect(() => {
-    // 未認証の場合スキップ
-    if (user) {
-      createNewFleet(user.uid).then((fleetRef) => {
+  useEffect(
+    () => {
+      // 未認証の場合スキップ
+      // if (user) {
+      // FIXME: firebase に依存しない実装に置き換える
+      createNewFleet("").then((fleetRef) => {
         replace(`/fleet/${fleetRef.id}`);
       });
-    }
-  }, [user]);
+      // }
+    },
+    [
+      /* user */
+    ]
+  );
 
   return null;
 };
