ac252e5e67e423b8308e3ddcf4ed7b794a08c653
Cano <developer.sasazono@gmail.com>
2023-07-14 17:24:34 +0900

    FleetListContainer 削除

--- src/components/home/fleet-list-area/index.tsx
+++ src/components/home/fleet-list-area/index.tsx
@@ -1,8 +1,42 @@
+import { Box, CircularProgress, Container } from "@mui/material";
 import { FC } from "react";
-import { FleetListContainer } from "../fleet-list-container";
+import { useEffectOnce } from "react-use";
+import { Fleet } from "~/models/fleet";
+import { EmptyState } from "../empty-state";
+import { FleetList } from "../fleet-list";
+import { useFleetList } from "../hooks";
+
+/**
+ * 保存されている編成が存在するか
+ */
+const checkExistFleetList = (fleets: Fleet[]) => {
+  return fleets.length !== 0;
+};
 
 export const FleetListArea: FC = () => {
   // const { data: signInCheckResult } = useSigninCheck();
+
+  const { data: fleetList, mutate: mutateFleetList } = useFleetList();
+
+  useEffectOnce(() => {
+    mutateFleetList([]);
+  });
+
+  if (!fleetList) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
+        <CircularProgress size={24} />
+      </Box>
+    );
+  }
+
+  // 保存されている編成が存在するか
+  const isExistFleetList = checkExistFleetList(fleetList);
 
   // if (!signInCheckResult.signedIn) {
   //   return (
@@ -12,5 +46,28 @@ export const FleetListArea: FC = () => {
   //   );
   // }
 
-  return <FleetListContainer />;
+  return (
+    <Container maxWidth="md" sx={{ height: "100%" }}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        paddingTop={3}
+        paddingBottom={3}
+        height="100%"
+      >
+        {isExistFleetList ? (
+          <FleetList fleetList={fleetList} />
+        ) : (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="100%"
+          >
+            <EmptyState />
+          </Box>
+        )}
+      </Box>
+    </Container>
+  );
 };
