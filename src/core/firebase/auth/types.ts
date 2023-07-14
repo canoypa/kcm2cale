cbd4456fda972de3599dca2f66c4c0b80d793f2d
Cano <developer.sasazono@gmail.com>
2023-07-14 17:53:03 +0900

    サインイン周りの機能を削除

--- src/core/firebase/auth/types.ts
+++
@@ -1,4 +0,0 @@
-import { ProviderId as AuthProviderId } from "firebase/auth";
-
-export const ProviderId = AuthProviderId;
-export type ProviderId = typeof ProviderId[keyof typeof ProviderId];
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,10 @@ import createEmotionCache, { EmotionCache } from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import { NextPage } from "next";
 import { AppProps } from "next/app";
-import dynamic from "next/dynamic";
 import Head from "next/head";
 import { PageLoadProgress } from "~/components/PageLoadProgress";
 import { ExtendedNextPage } from "~/types/next-page";
 import { ThemeProvider } from "../components/providers/theme-provider";
-
-const AuthProvider = dynamic(
-  () => import("~/components/providers/auth-provider")
-);
 
 const clientSideEmotionCache = createEmotionCache({
   key: "css",
@@ -39,7 +34,6 @@ const App: NextPage<Props> = ({
       <CacheProvider value={emotionCache}>
         <ThemeProvider>
           {/* <CssBaseline /> */}
-          <AuthProvider />
 
           <PageLoadProgress />
           {getSharedLayout(<Component {...pageProps} />)}
