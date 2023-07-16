d5bc6010199f5a6827ae8729ed099c432208294d
Cano <developer.sasazono@gmail.com>
2023-07-16 13:14:48 +0900

    delete firebase

--- src/core/firebase/sdk/analytics.ts
+++
@@ -1,6 +0,0 @@
-import { getAnalytics as originalGetAnalytics } from "firebase/analytics";
-import { firebaseApp } from "./app";
-
-export const getAnalytics = () => originalGetAnalytics(firebaseApp);
-
-if (typeof window !== "undefined") getAnalytics();
--- src/models/firestore/ship.ts
+++
@@ -1,9 +0,0 @@
-import { array, literal, number, object, string, union } from "zod";
-
-export const ShipDoc = object({
-  fleetNo: union([literal(0), literal(1)]),
-  turnNo: number().min(0).max(6),
-  no: string(),
-});
-
-export const ShipDocs = array(ShipDoc);
