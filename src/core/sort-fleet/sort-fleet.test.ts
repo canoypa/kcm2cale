d0c00f70bb17ba962c011f9bb317474afa0cd595
Cano <developer.sasazono@gmail.com>
2023-07-17 20:24:06 +0900

    fix test

--- src/core/sort-fleet/sort-fleet.test.ts
+++ src/core/sort-fleet/sort-fleet.test.ts
@@ -3,15 +3,15 @@ import { FleetShip } from "../../models/ship";
 
 const fleetData: FleetShip[] = [
   { fleetNo: 0, turnNo: 0, no: "0", equipments: [] },
-  { fleetNo: 0, turnNo: 1, no: "0", equipments: [] },
-  { fleetNo: 0, turnNo: 2, no: "0", equipments: [] },
+  { fleetNo: 0, turnNo: 1, no: "1", equipments: [] },
+  { fleetNo: 0, turnNo: 2, no: "2", equipments: [] },
 ];
 
 describe("SortFleet", () => {
   test("Single Move", () => {
     const expected = expect.arrayContaining([
-      expect.objectContaining({ turnNo: 0, id: "1" }),
-      expect.objectContaining({ turnNo: 1, id: "0" }),
+      expect.objectContaining({ turnNo: 0, no: "1" }),
+      expect.objectContaining({ turnNo: 1, no: "0" }),
     ]);
 
     expect(sortFleet(fleetData, 0, 0, 1)).toEqual(expected);
@@ -19,8 +19,8 @@ describe("SortFleet", () => {
 
   test("Reversed Single Move", () => {
     const expected = expect.arrayContaining([
-      expect.objectContaining({ turnNo: 1, id: "0" }),
-      expect.objectContaining({ turnNo: 0, id: "1" }),
+      expect.objectContaining({ turnNo: 1, no: "0" }),
+      expect.objectContaining({ turnNo: 0, no: "1" }),
     ]);
 
     expect(sortFleet(fleetData, 0, 1, 0)).toEqual(expected);
@@ -28,9 +28,9 @@ describe("SortFleet", () => {
 
   test("Multi Move", () => {
     const expected = expect.arrayContaining([
-      expect.objectContaining({ turnNo: 0, id: "1" }),
-      expect.objectContaining({ turnNo: 1, id: "2" }),
-      expect.objectContaining({ turnNo: 2, id: "0" }),
+      expect.objectContaining({ turnNo: 0, no: "1" }),
+      expect.objectContaining({ turnNo: 1, no: "2" }),
+      expect.objectContaining({ turnNo: 2, no: "0" }),
     ]);
 
     expect(sortFleet(fleetData, 0, 0, 2)).toEqual(expected);
@@ -38,9 +38,9 @@ describe("SortFleet", () => {
 
   test("Reversed Multi Move", () => {
     const expected = expect.arrayContaining([
-      expect.objectContaining({ turnNo: 0, id: "2" }),
-      expect.objectContaining({ turnNo: 1, id: "0" }),
-      expect.objectContaining({ turnNo: 2, id: "1" }),
+      expect.objectContaining({ turnNo: 0, no: "2" }),
+      expect.objectContaining({ turnNo: 1, no: "0" }),
+      expect.objectContaining({ turnNo: 2, no: "1" }),
     ]);
 
     expect(sortFleet(fleetData, 0, 2, 0)).toEqual(expected);
