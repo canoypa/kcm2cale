154810e8d238d41286e0b5030afa3b15ca35efa3
Cano <developer.sasazono@gmail.com>
2023-07-22 15:00:30 +0900

    migrate new/ to app router

---
+++ src/app/new/page.tsx
@@ -0,0 +1,5 @@
+import NewFleet from '../../components/new-fleet'
+
+export default function NewFleetPage() {
+  return <NewFleet />
+}
--- src/components/new-fleet/index.tsx
+++ src/components/new-fleet/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { FC } from 'react'
 import { useEffectOnce } from 'react-use'
 import { useCreateNewFleet } from './useCreateNewFleet'
