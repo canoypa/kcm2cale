b44225c4b2a089dc88bc41b8c42c123cf29af93b
Cano <developer.sasazono@gmail.com>
2023-10-05 14:46:55 +0900

    update schema to parse json string

--- src/core/persistence/schema.ts
+++ src/core/persistence/schema.ts
@@ -1,30 +1,60 @@
-import { array, coerce, literal, number, object, string, union } from 'zod'
+import {
+  NEVER,
+  any,
+  array,
+  coerce,
+  literal,
+  number,
+  object,
+  string,
+  union,
+} from 'zod'
 
-export const ExportedFleetSchema = object({
-  version: literal(1),
-  type: union([
-    literal('Normal'),
-    literal('Carrier'),
-    literal('Surface'),
-    literal('Transport'),
-    literal('StrikingForce'),
-  ]),
-  title: string(),
-  description: string(),
-  createdAt: coerce.date(),
-  updatedAt: coerce.date(),
+// util
+const coerceJson = union([
+  // 文字列なら json としてパースする
+  string().transform((str, ctx) => {
+    try {
+      return JSON.parse(str)
+    } catch (e) {
+      ctx.addIssue({ code: 'custom', message: 'Invalid JSON' })
+      return NEVER
+    }
+  }),
+  any(),
+])
 
-  ships: array(
-    object({
-      fleetNo: number(),
-      turnNo: number(),
-      no: string(),
-      equipments: array(
-        object({
-          slotNo: number(),
-          no: number(),
-        }),
-      ),
-    }),
-  ),
-})
+export const ExportedFleetSchema = coerceJson.pipe(
+  object({
+    version: literal(1),
+    type: union([
+      literal('Normal'),
+      literal('Carrier'),
+      literal('Surface'),
+      literal('Transport'),
+      literal('StrikingForce'),
+    ]),
+    title: string(),
+    description: string(),
+    createdAt: coerce.date(),
+    updatedAt: coerce.date(),
+
+    ships: array(
+      object({
+        fleetNo: number(),
+        turnNo: number(),
+        no: string(),
+        equipments: array(
+          object({
+            slotNo: number(),
+            no: number(),
+          }),
+        ),
+      }),
+    ),
+  }),
+)
+
+export const ExportedFleetListSchema = coerceJson.pipe(
+  array(ExportedFleetSchema),
+)
