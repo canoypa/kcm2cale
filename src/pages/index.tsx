798e5c0c477c9720468d4220796bbebfccb2768b
Cano <developer.sasazono@gmail.com>
2023-10-08 16:13:47 +0900

    show banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Box, Grid } from '@mui/material'
+import { Alert, AlertTitle, Box, Container, Grid } from '@mui/material'
 import { NextPage } from 'next'
 import Head from 'next/head'
+import { TextLink } from '~/components/common/TextLink'
 import { ExportFleetDialog } from '~/components/export/fleet_export_dialog'
 import { ImportFleetDialog } from '~/components/import/fleet_import_dialog'
 import { MainAppBar } from '../components/common/main-app-bar'
@@ -9,6 +10,10 @@ import { FleetListArea } from '../components/home/fleet-list-area'
 import { APP_NAME } from '../core/env'
 
 const HomePage: NextPage = () => {
+  const isOldSite =
+    typeof window !== 'undefined' &&
+    window.location.hostname !== 'kcm2cale.tepbyte.dev'
+
   return (
     <>
       <Head>
@@ -31,6 +36,21 @@ const HomePage: NextPage = () => {
         <Grid container direction="column" wrap="nowrap" height="100vh">
           <MainAppBar />
           <Box flexGrow={1}>
+            {isOldSite && (
+              <Container maxWidth="md">
+                <Alert severity="warning" variant="outlined">
+                  <AlertTitle>
+                    このサイトは{' '}
+                    <TextLink ext href="https://kcm2cale.tepbyte.dev">
+                      kcm2cale.tepbyte.dev
+                    </TextLink>{' '}
+                    に移転しました。
+                  </AlertTitle>
+                  右上のメニューから編成のエクスポート/インポートを行い、新しいサイトをご利用ください。
+                </Alert>
+              </Container>
+            )}
+
             <FleetListArea />
           </Box>
           <CreateNewFleet />
