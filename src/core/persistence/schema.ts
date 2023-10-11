37be27cdd27a000099fe489875040144bf48ed22
Cano <developer.sasazono@gmail.com>
2023-10-11 09:49:34 +0900

    parse fleet data on load (#158)

--- src/core/persistence/schema.ts
+++ src/core/persistence/schema.ts
@@ -1,58 +1,40 @@
-import {
-  NEVER,
-  any,
-  array,
-  coerce,
-  literal,
-  number,
-  object,
-  string,
-  union,
-} from 'zod'
+import type * as zod from 'zod'
+import { array, coerce, literal, number, object, string, union } from 'zod'
+import { coerceJson } from '~/util/zod'
 
-// util
-const coerceJson = union([
-  // 文字列なら json としてパースする
-  string().transform((str, ctx) => {
-    try {
-      return JSON.parse(str)
-    } catch (e) {
-      ctx.addIssue({ code: 'custom', message: 'Invalid JSON' })
-      return NEVER
-    }
-  }),
-  any(),
-])
+export const FleetSchema = object({
+  id: string().length(16),
+  version: literal(1),
+  type: union([
+    literal('Normal'),
+    literal('Carrier'),
+    literal('Surface'),
+    literal('Transport'),
+    literal('StrikingForce'),
+  ]),
+  title: string(),
+  description: string(),
+  createdAt: coerce.date(),
+  updatedAt: coerce.date(),
+
+  ships: array(
+    object({
+      fleetNo: number(),
+      turnNo: number(),
+      no: string(),
+      equipments: array(
+        object({
+          slotNo: number(),
+          no: number(),
+        }),
+      ),
+    }),
+  ),
+})
+export type FleetSchema = zod.infer<typeof FleetSchema>
 
 export const ExportedFleetSchema = coerceJson.pipe(
-  object({
-    version: literal(1),
-    type: union([
-      literal('Normal'),
-      literal('Carrier'),
-      literal('Surface'),
-      literal('Transport'),
-      literal('StrikingForce'),
-    ]),
-    title: string(),
-    description: string(),
-    createdAt: coerce.date(),
-    updatedAt: coerce.date(),
-
-    ships: array(
-      object({
-        fleetNo: number(),
-        turnNo: number(),
-        no: string(),
-        equipments: array(
-          object({
-            slotNo: number(),
-            no: number(),
-          }),
-        ),
-      }),
-    ),
-  }),
+  FleetSchema.omit({ id: true }),
 )
 
 export const ExportedFleetListSchema = coerceJson.pipe(
